22c88be54a08b40b1d2cc00cd409bceb8bccfd84
Young.Geo <anxan524@126.com>
2017-07-23 14:59:51 +0800

    更改类名

---
+++ Yprocess.cpp
@@ -0,0 +1,64 @@
+#include "process.h"
+
+
+
+int work(void *arg, xchain *rchain, xchain *wchain)
+{
+	return 0;
+}
+
+int do_work(void *arg, void *r, void *w)
+{
+
+	thread_entity_t *thread_entity = NULL;
+	xchain *rchain = NULL, *wchain = NULL;
+	unsigned char type = '\0';
+
+
+	assert(arg);
+	assert(r);
+	assert(w);
+
+
+	thread_entity = (thread_entity_t *)arg;
+	rchain = (xchain *)r, wchain = (xchain *)w;
+
+	xchain_get(rchain, (void *)&type, 1);
+	
+
+	switch (type)//各种业务处理
+	{
+		case 'C':
+			//登录处理
+		break;
+		
+		default:
+			
+		break;
+	}
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

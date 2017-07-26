29358e2e0962f7368695a372cd042ecc3d5782bb
Young.Geo <anxan524@126.com>
2017-07-27 00:12:43 +0800

    提交修改

--- Server/Yprocess.cpp
+++ Server/Yprocess.cpp
@@ -100,7 +100,7 @@ int do_work(void *arg, void *r, void *w)
 				//xchain_get(rchain, (void *)username, USERNAME_LEN);				
 				//xchain_get(rchain, (void *)password, PASSWORD_LEN);
 				memcpy((void *)username, (data + 2), USERNAME_LEN);				
-				memcpy((void *)username, (data + USERNAME_LEN + 2), PASSWORD_LEN);
+				memcpy((void *)password, (data + USERNAME_LEN + 2), PASSWORD_LEN);
 				if (login(thread_entity, username, password)) {
 					xerror("login error\n");
 					}

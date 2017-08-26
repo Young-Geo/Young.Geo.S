ded595c0299bb22e1d2561dc3d656478b9cc9e21
Young.Geo <anxan524@126.com>
2017-08-26 10:11:29 +0800

    修改乱码错误

--- Server/Yprocess.cpp
+++ Server/Yprocess.cpp
@@ -182,7 +182,7 @@ int do_work(void *arg, void *r, void *w)
 				
 				xchain_get(rchain, (void *)username, USERNAME_LEN);	
 				xchain_delete(rchain, USERNAME_LEN);
-				xchain_get(rchain, (void *)password, PASSWORD_LEN);
+				xchain_get(rchain, (void *)password, (PASSWORD_LEN - 1));
 				xchain_delete(rchain, PASSWORD_LEN);
 
 				if (!(u_buf = (u8 *)login(thread_entity, username, password))) {

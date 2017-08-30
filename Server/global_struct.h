485cd7b992e1e57f405b216d5edc05a282f7a0d3
Young.Geo <anxan524@126.com>
2017-08-30 17:50:16 +0800

    G

--- Server/global_struct.h
+++ Server/global_struct.h
@@ -2,6 +2,13 @@
 #define _GLOBAL_STRUCT_H_
 
 #include "ocicursor.h"
+#include <event2/listener.h>
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <event2/event.h>
+#include <event2/event_struct.h>
+#include <event2/event_compat.h>
+
 
 
 

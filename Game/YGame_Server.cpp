42e89c43ab0d8c411ec2b170fdd237b993f6fe87
Young.Geo <anxan524@126.com>
2017-08-31 18:13:17 +0800

    G

--- Game/YGame_Server.cpp
+++ Game/YGame_Server.cpp
@@ -9,14 +9,14 @@
 #define SERV_PORT   8000
 /* * status:1表示在监听事件中，0表示不在  * last_active:记录最后一次响应时间,做超时处理 */
 struct myevent_s {
-	int fd;
-	int events;
-	void *arg;
-	void (*call_back)(int fd, int events, void *arg);
-	int status;
-	char buf[BUFLEN];
-	int len;
-	long last_active;
+    int fd;
+    int events;
+    void *arg;
+    void (*call_back)(int fd, int events, void *arg);
+    int status;
+    char buf[BUFLEN];
+    int len;
+    long last_active;
 };
 int g_efd;
 /* epoll_create返回的句柄 */
@@ -24,99 +24,99 @@ struct myevent_s g_events[MAX_EVENTS+1];
 /* +1 最后一个用于 listen fd */
 void eventset(struct myevent_s *ev, int fd, void (*call_back)(int, int, void *), void *arg)
 {
-	ev->fd = fd;
-	ev->call_back = call_back;
-	ev->events = 0;
-	ev->arg = arg;
-	ev->status = 0;
-	//memset(ev->buf, 0, sizeof(ev->buf));
-	//ev->len = 0;
-	ev->last_active = time(NULL);
-	return;
+    ev->fd = fd;
+        ev->call_back = call_back;
+    ev->events = 0;
+    ev->arg = arg;
+    ev->status = 0;
+    //memset(ev->buf, 0, sizeof(ev->buf));
+    //ev->len = 0;
+    ev->last_active = time(NULL);
+    return;
 }
 
 void recvdata(int fd, int events, void *arg);
 void senddata(int fd, int events, void *arg);
 void eventadd(int efd, int events, struct myevent_s *ev)
 {
-	struct epoll_event epv = {0, {0}};
-	int op;
-	epv.data.ptr = ev;
-	epv.events = ev->events = events;
+    struct epoll_event epv = {0, {0}};
+    int op;
+    epv.data.ptr = ev;
+    epv.events = ev->events = events;
 
-	if (ev->status == 1) {
-		op = EPOLL_CTL_MOD;
-	} else {
-		op = EPOLL_CTL_ADD;
-		ev->status = 1;
-	}
+    if (ev->status == 1) {
+        op = EPOLL_CTL_MOD;
+    } else {
+        op = EPOLL_CTL_ADD;
+        ev->status = 1;
+    }
 
 
-	if (epoll_ctl(efd, op, ev->fd, &epv) < 0)
-		printf("event add failed [fd=%d], events[%d]\n", ev->fd, events);
-	else
-		printf("event add OK [fd=%d], op=%d, events[%0X]\n", ev->fd, op, events);  
-	return;
+    if (epoll_ctl(efd, op, ev->fd, &epv) < 0)
+        printf("event add failed [fd=%d], events[%d]\n", ev->fd, events);
+    else
+        printf("event add OK [fd=%d], op=%d, events[%0X]\n", ev->fd, op, events);  
+    return;
 }
 
 
 void eventdel(int efd, struct myevent_s *ev)
 {
-	struct epoll_event epv = {0,  { 0 }};
-	if (ev->status != 1)
-		return;
-	epv.data.ptr = ev;
-	ev->status = 0;
-	epoll_ctl(efd, EPOLL_CTL_DEL, ev->fd, &epv);
-	return;
+    struct epoll_event epv = {0,  { 0 }};
+    if (ev->status != 1)
+        return;
+    epv.data.ptr = ev;
+    ev->status = 0;
+    epoll_ctl(efd, EPOLL_CTL_DEL, ev->fd, &epv);
+    return;
 }
 void acceptconn(int lfd, int events, void *arg)
 {
-	struct sockaddr_in cin;
-	socklen_t len = sizeof(cin);
-	int cfd, i;
-
-	
-	if ((cfd = accept(lfd, (struct sockaddr *)&cin, &len)) == -1) {
-
-		if (errno != EAGAIN && errno != EINTR) { 
-			/* 暂时不做出错处理 */
-		}
-		
-		xprintf("%s: accept, %s\n", __func__, strerror(errno));
-		return;
-	}
-
-	
-	do {
-
-		for (i = 0; i < MAX_EVENTS; i++)
-		{
-			if (g_events[i].status == 0)
-				break;
-		}
+    struct sockaddr_in cin;
+    socklen_t len = sizeof(cin);
+    int cfd, i;
 
 
-		if (i == MAX_EVENTS) {
+    if ((cfd = accept(lfd, (struct sockaddr *)&cin, &len)) == -1) {
 
-			xprintf("%s: max connect limit[%d]\n", __func__, MAX_EVENTS);
-			break;
-			
-		}
-		
-		int flag = 0;
-		if ((flag = fcntl(cfd, F_SETFL, O_NONBLOCK)) < 0) {
-			xprintf("%s: fcntl nonblocking failed, %s\n", __func__, strerror(errno));
-			break;
-		}
+        if (errno != EAGAIN && errno != EINTR) { 
+            /* 暂时不做出错处理 */
+        }
 
-		eventset(&g_events[i], cfd, recvdata, &g_events[i]);
-		eventadd(g_efd, EPOLLIN, &g_events[i]);
-		
-	} while(0);
-	
-	xprintf("new connect [%s:%d][time:%ld], pos[%d]\n", inet_ntoa(cin.sin_addr), ntohs(cin.sin_port), g_events[i].last_active, i); 
-	return;
+        xprintf("%s: accept, %s\n", __func__, strerror(errno));
+        return;
+    }
+
+
+    do {
+
+        for (i = 0; i < MAX_EVENTS; i++)
+        {
+            if (g_events[i].status == 0)
+                break;
+        }
+
+
+        if (i == MAX_EVENTS) {
+
+            xprintf("%s: max connect limit[%d]\n", __func__, MAX_EVENTS);
+            break;
+
+        }
+
+        int flag = 0;
+        if ((flag = fcntl(cfd, F_SETFL, O_NONBLOCK)) < 0) {
+            xprintf("%s: fcntl nonblocking failed, %s\n", __func__, strerror(errno));
+            break;
+        }
+
+        eventset(&g_events[i], cfd, recvdata, &g_events[i]);
+        eventadd(g_efd, EPOLLIN, &g_events[i]);
+
+    } while(0);
+
+    xprintf("new connect [%s:%d][time:%ld], pos[%d]\n", inet_ntoa(cin.sin_addr), ntohs(cin.sin_port), g_events[i].last_active, i); 
+    return;
 }
 
 
@@ -131,23 +131,23 @@ void acceptconn(int lfd, int events, void *arg)
 
 void recvdata(int fd, int events, void *arg)
 {
-	struct myevent_s *ev = (struct myevent_s *)arg;
-	int len;
+    struct myevent_s *ev = (struct myevent_s *)arg;
+    int len;
 
-	len = recv(fd, ev->buf, sizeof(ev->buf), 0);
-	eventdel(g_efd, ev);
+    len = recv(fd, ev->buf, sizeof(ev->buf), 0);
+    eventdel(g_efd, ev);
 
-	if (len > 0) {
-		ev->len = len;
-		ev->buf[len] = '\0';
-		xprintf("C[%d]:%s\n", fd, ev->buf);
-		/* 转换为发送事件 */
-		eventset(ev, fd, senddata, ev);
-		eventadd(g_efd, EPOLLOUT, ev);
-		}
-	else if (len == 0) {        close(ev->fd);        /* ev-g_events 地址相减得到偏移元素位置 */        printf("[fd=%d] pos[%d], closed\n", fd, ev - g_events);    }    else {        close(ev->fd);        printf("recv[fd=%d] error[%d]:%s\n", fd, errno, strerror(errno));    }    return;}void senddata(int fd, int events, void *arg){    struct myevent_s *ev = (struct myevent_s *)arg;    int len;    //处理业务，小写转大写    len = send(fd, ev->buf, ev->len, 0);    /*    printf("fd=%d\tev->buf=%s\ttev->len=%d\n", fd, ev->buf, ev->len);    printf("send len = %d\n", len);    */    if (len > 0) {        printf("send[fd=%d], [%d]%s\n", fd, len, ev->buf);        eventdel(g_efd, ev);        eventset(ev, fd, recvdata, ev);        eventadd(g_efd, EPOLLIN, ev);    }    else {        close(ev->fd);        eventdel(g_efd, ev);        printf("send[fd=%d] error %s\n", fd, strerror(
-errno));
-}    return;}
+    if (len > 0) {
+        ev->len = len;
+        ev->buf[len] = '\0';
+        xprintf("C[%d]:%s\n", fd, ev->buf);
+        /* 转换为发送事件 */
+        eventset(ev, fd, senddata, ev);
+        eventadd(g_efd, EPOLLOUT, ev);
+    }
+    else if (len == 0) {        close(ev->fd);        /* ev-g_events 地址相减得到偏移元素位置 */        printf("[fd=%d] pos[%d], closed\n", fd, ev - g_events);    }    else {        close(ev->fd);        printf("recv[fd=%d] error[%d]:%s\n", fd, errno, strerror(errno));    }    return;}void senddata(int fd, int events, void *arg){    struct myevent_s *ev = (struct myevent_s *)arg;    int len;    //处理业务，小写转大写    len = send(fd, ev->buf, ev->len, 0);    /*    printf("fd=%d\tev->buf=%s\ttev->len=%d\n", fd, ev->buf, ev->len);    printf("send len = %d\n", len);    */    if (len > 0) {        printf("send[fd=%d], [%d]%s\n", fd, len, ev->buf);        eventdel(g_efd, ev);        eventset(ev, fd, recvdata, ev);        eventadd(g_efd, EPOLLIN, ev);    }    else {        close(ev->fd);        eventdel(g_efd, ev);        printf("send[fd=%d] error %s\n", fd, strerror(
+        errno));
+    }    return;}
 
 
 
@@ -160,19 +160,19 @@ void initlistensocket(int efd, short port){    int lfd = socket(AF_INET, SOCK_ST
 
 static void *game_work(void *arg)
 {
-	
-	return NULL;
+
+    return NULL;
 }
 
 
 int YS_game_thread_init(global_t *master)
 {
-	int ret = 0;
-	if ((ret = pthread_create(&master->game_thread_id, NULL, game_work, master)) != 0) {
+    int ret = 0;
+    if ((ret = pthread_create(&master->game_thread_id, NULL, game_work, master)) != 0) {
         xerror("Can't create thread: %s",
                 strerror(ret));
         exit(1);
     }
-	return 0;
+    return 0;
 }
 
